4f7fae8e41a13107ba137f8e15fa849217f4f41e
Nelson <g00412122@atu.ie>
2023-12-01 10:47:25 +0000

    lab8

--- src/components/books.js
+++ src/components/books.js
@@ -1,16 +1,13 @@
-// Importing the BookItem component from the "./bookItem" file
 import BookItem from "./bookItem";
 
-// Defining a functional component called Books, which takes 'props' as input
-function Books(props) {
-    // Using the map function to iterate through the 'myBooks' array passed in props
-    // For each 'book' in the 'myBooks' array, rendering a BookItem component with specific props
+function Books(props){
+
     return props.myBooks.map(
         (book)=>{
-            return <BookItem myBook={book} key={book._id}></BookItem>
+            return <BookItem myBook={book} key={book._id} reload={()=>{props.Reload();}}></BookItem>
         }
     );
+
 }
 
-// Exporting the Books component as default
 export default Books;
--- src/components/read.js
+++ src/components/read.js
@@ -1,79 +1,50 @@
-// Defining the Read component
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Books from "./books";
+
 function Read() {
-<<<<<<< HEAD
-
+   
     const [data, setData] = useState([]);
 
-    useEffect(
-        () => {
-
-            axios.get('http://localhost:4000/api/books')
-                .then(
-                    (response) => {
-                        setData(response.data)
-                    }
-                )
-                .catch(
-                    (error) => {
-                        console.log(error);
-                    }
-                )
-
-        }, []
-    );
-
-    const ReloadData = (e) => {
+  useEffect(
+    ()=>{
+        
         axios.get('http://localhost:4000/api/books')
-            .then(
-                (response) => {
-                    setData(response.data)
-                }
-            )
-            .catch(
-                (error) => {
-                    console.log(error);
-                }
-            )
+        .then(
+            (response)=>{
+                setData(response.data)
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error);
+            }
+        )
 
+    }, []
+  );
+
+  const ReloadData = (e)=>{
+        axios.get('http://localhost:4000/api/books')
+        .then(
+            (response)=>{
+                setData(response.data)
+            }
+        )
+        .catch(
+            (error)=>{
+                console.log(error);
+            }
+        )
     }
-=======
-    // Initializing state for data using the useState hook, setting it as an empty array initially
-    const [data, setData] = useState([]);
 
-    // Using the useEffect hook to perform an action on component mount
-    useEffect(
-        () => {
-            // Making a GET request to fetch data from the specified API endpoint
-            axios.get('http://localhost:4000/api/books')
-                .then(
-                    // Handling the response by updating the state with the received data
-                    (response) => {
-                        setData(response.data);
-                    }
-                )
-                .catch(
-                    // Handling errors in case of failed requests
-                    (error) => {
-                        console.log(error);
-                    }
-                );
-        }, [] // Passing an empty dependency array to ensure this effect runs only once on mount
-    );
->>>>>>> 85ed455be2c22a252e2d3f390aa73fee0e578d56
-
-    // Rendering content, including the Books component with the fetched data as a prop
     return (
         <div>
             <h2>Hello from Read Component!</h2>
-<<<<<<< HEAD
-            <Books myBooks={data} Reload={ReloadData}></Books>
-=======
-            {/* Passing 'data' as props to the Books component */}
-            <Books myBooks={data}></Books>
->>>>>>> 85ed455be2c22a252e2d3f390aa73fee0e578d56
+            <Books myBooks={data} Reload = {ReloadData}></Books>
         </div>
     );
+
 }
 
-// Exporting the Read component as default
 export default Read;
